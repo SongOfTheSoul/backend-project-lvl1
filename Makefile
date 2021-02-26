5e692c676431b78b29fc1dfebf1f4d096b404f50
SongOf_TheSoul <kindret93@gmail.com>
2021-02-27 01:48:59 +0300

    6

--- Makefile
+++ Makefile
@@ -11,3 +11,9 @@ publish:
 
 lint:
 	npx eslint .
+
+brain-even:
+	node bin/brain-even.js
+
+brain-calc:
+	node bin/brain-calc.js
---
+++ bin/brain-calc.js
@@ -0,0 +1,58 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable quotes */
+import readlineSync from 'readline-sync';
+import cli from '../src/cli.js';
+
+const calc = (name) => {
+  console.log(`What is the result of the expression?`);
+  let step = 0;
+  while (step < 3) {
+    const num1 = Math.floor((Math.random() * 100) + 1);
+    const num2 = Math.floor((Math.random() * 3) + 1);
+    const num3 = Math.floor((Math.random() * 100) + 1);
+    switch (num2) {
+      case 1: {
+        console.log(`Question: ${num1} + ${num3}`);
+        const ans = readlineSync.question('Answer: ');
+        if (ans == (num1 + num3)) {
+          step += 1;
+          console.log(`Correct!`);
+        } else {
+          step = 0;
+          console.log(`${ans} is wrong answer ;(. Correct answer was ${num1 + num3}.`);
+          console.log(`Let's try again, ${name}!`);
+        }
+        break;
+      }
+      case 2: {
+        console.log(`Question: ${num1} - ${num3}`);
+        const ans = readlineSync.question('Answer: ');
+        if (ans == (num1 - num3)) {
+          step += 1;
+          console.log(`Correct!`);
+        } else {
+          step = 0;
+          console.log(`${ans} is wrong answer ;(. Correct answer was ${num1 - num3}.`);
+          console.log(`Let's try again, ${name}!`);
+        }
+        break;
+      }
+      default: {
+        console.log(`Question: ${num1} * ${num3}`);
+        const ans = readlineSync.question('Answer: ');
+        if (ans == (num1 * num3)) {
+          step += 1;
+          console.log(`Correct!`);
+        } else {
+          step = 0;
+          console.log(`${ans} is wrong answer ;(. Correct answer was ${num1 * num3}.`);
+          console.log(`Let's try again, ${name}!`);
+        }
+      }
+    }
+  }
+  console.log(`Congratulations, ${name}`);
+};
+console.log('Welcome to the Brain Games!');
+const name = cli();
+calc(name);
